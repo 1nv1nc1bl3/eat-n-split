fe96223d96e796e1643a53819280b6984b158cff
Alkis <alkis.k1972@gmail.com>
2025-10-03 19:24:07 +0300

    changed favicon

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import Button from './Button';
 import FriendsList from './FriendsList';
 import FormAddFriend from './FormAddFriend';
 import FormSplitBill from './FormSplitBill';
-
-const initialFriends = [
-    {
-        id: 118836,
-        name: 'Clark',
-        image: 'https://i.pravatar.cc/48?u=118836',
-        balance: -7,
-    },
-    {
-        id: 933372,
-        name: 'Sarah',
-        image: 'https://i.pravatar.cc/48?u=933372',
-        balance: 20,
-    },
-    {
-        id: 499476,
-        name: 'Anthony',
-        image: 'https://i.pravatar.cc/48?u=499476',
-        balance: 0,
-    },
-];
+import { initialFriends } from './data';
 
 export default function App() {
     const [friends, setFriends] = useState(initialFriends);
